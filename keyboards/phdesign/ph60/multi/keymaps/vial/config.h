91dec565317335a0cf4d2228c03b2c6a63de4934
alt-0191 <2223147307@qq.com>
2025-11-30 01:10:18 +0800

    Add ph60 multi

---
+++ keyboards/phdesign/ph60/multi/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x70, 0xA0, 0xB3, 0xC9, 0x6F, 0x07, 0x39, 0x27}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2}
+#define VIAL_UNLOCK_COMBO_COLS {0, 12}
